4405883034f4d5363792f68d278404b582192d47
gcopar <gcopar@unjbg.edu.pe>
2024-07-01 05:13:44 -0500

    Agregar ejercicio04.cpp

---
+++ Funciones II/ejercicio04.cpp
@@ -0,0 +1,45 @@
+//EJERCICIO 04: CALCULO DE RAICES DE UNA ECUACION CUADRATICA
+//NOMBRE: Gustavo Mario de la Cruz Copa Romero
+//CODIGO: 2021-177006
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a, b, c, discr;
+    float x_1, x_2, preal, pimgn;
+
+    cout<<"Ingrese el coeficiente de x^2: "; cin>>a;
+    cout<<"Ingrese el coeficiente de x: "; cin>>b;
+    cout<<"Ingrese la constante: "; cin>>c;
+
+    discr = pow(b, 2) - 4*a*c;
+
+    if (discr > 0)
+    {
+        x_1 = (-b + sqrt(discr)) / (2*a);
+        x_2 = (-b - sqrt(discr)) / (2*a);
+
+        cout<<"RAICES: "<<endl;
+        cout<<"\t - x_1 = "<<x_1<<endl;
+        cout<<"\t - x_2 = "<<x_2<<endl;
+    }else if (discr == 0)
+    {
+        x_1 = -b / (2*a);
+
+        cout<<"RAIZ: "<<endl;
+        cout<<"\t - x_1 = "<<x_1<<endl;
+    }else{
+        preal = -b / (2*a);
+        pimgn = sqrt(abs(discr)) / (2*a);
+
+        cout<<"RAICES: "<<endl;
+        cout<<"\t - x_1 = "<<preal<<" + "<<pimgn<<"i"<<endl;
+        cout<<"\t - x_2 = "<<preal<<" - "<<pimgn<<"i"<<endl;
+    }
+
+    return 0;
+}
