e9ec8f812603072200a849871f2df896d5ee2363
gcopar <gcopar@unjbg.edu.pe>
2024-07-01 02:22:05 -0500

    Agregar carpeta Funciones II y ejercicio01.cpp

---
+++ Funciones II/ejercicio01.cpp
@@ -0,0 +1,17 @@
+//EJERCICIO01: CALCULO DEL AREA DE UN CIRCULO
+//NOMBRE: Gustavo Mario de la Cruz Copa Romero
+//CODIGO: 2021-177006
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    float radio, resultado;
+
+    cout<<"Ingrese el radio de circulo: "; cin>>radio;
+    resultado = M_PI * pow(2, radio);
+
+    cout<<"El area del circulo es: "<<resultado<<endl;
+}
